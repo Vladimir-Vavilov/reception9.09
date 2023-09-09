dbb312cf8d304d2c4c5d0c1cc9a69b4ab163e9a2
Vavilov <red_white_94@mail.rugit config --global --listgit config --global user.name Vavilov>
2023-09-09 18:15:49 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+from pprint import pprint
+
+
+def op_file():
+    with open('reception.txt', 'rt') as f:
+        cook_book = {}
+        for line in f:
+            eat_name = line.strip()
+            ingredient_name = int(f.readline())
+            meas = []
+        for i in range(ingredient_name):
+            mea = f.readline()
+            ingredient, quantity, measure = mea.split(' | ')
+            meas.append({
+                'ingredient_name': ingredient,
+                'quantity': quantity,
+                'measure': measure
+            })
+        f.readline()
+        cook_book[eat_name] = meas
+    pprint(cook_book)
+
+
+def my_cook_book():
+    with open('reception.txt', encoding='utf-8') as file:
+        cook_book = {}
+        for txt in file.read().split('\n\n'):
+            name, _, *args = txt.split('\n')
+            tmp = []
+            for arg in args:
+                ingredient_name, quantity, measure = map(lambda x: int(x) if x.isdigit() else x, arg.split(' | '))
+                tmp.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+            cook_book[name] = tmp
+    return cook_book
+
+
+def create_shop_list():
+    person_count = int(input('Введите количество человек: '))
+    dishes = input('Введите блюдо: ') \
+        .capitalize().split(', ')
+
+    shop_list = get_shop_list_by_dishes(dishes, person_count)
+
+    return shop_list
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    my_cookbook = my_cook_book()
+    for dish in dishes:
+        if dish not in my_cookbook:
+            print(f"Блюдо '{dish}' не найдено в книге рецептов.")
+            continue
+        for ingredient in my_cookbook[dish]:
+            ingredient_name = ingredient['ingredient_name']
+            quantity = ingredient['quantity'] * person_count
+            if ingredient_name in shop_list:
+                shop_list[ingredient_name] += quantity
+            else:
+                shop_list[ingredient_name] = quantity
+        return shop_list
+
+
+pprint(create_shop_list())
